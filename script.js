09ca08b03afc647af98a02ca10ab1ffd1ca50cd7
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-04-10 16:37:07 +0200

    Assignment completed

--- script.js
+++ script.js
@@ -7,51 +7,81 @@ function getComputerChoice() {
     else return "paper";
 }
 
-// function to get the user's choice between rock, paper and scissors
-function getHumanChoice() {
-    return prompt("Please enter your choice (rock/paper/scissors)");
-}
-
-//get the computer and user's choices // not needed anymore
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-
 
 function playGame() {
     //declare the players' score initializing with 0
-    let computerScore = 0;
     let humanScore = 0;
+    let computerScore = 0;
     
     //logic to play a single round
-    function playRound(humanChoice, computerChoice) {
+    function playRound(playerSelection, computerChoice) {
         
-        humanChoice = humanChoice.toLowerCase(); //human's choice to lowercase for case insensitivity of user input
+        playerSelection = playerSelection.toLowerCase(); //human's choice to lowercase for case insensitivity of user input
         
         // Determine if it's a draw and return msg
-        if (humanChoice == computerChoice) {
-            console.log("It's a draw!")
+        if (playerSelection == computerChoice) {
+            roundResult.textContent = "It's a draw!";
+            humanScoreSpan.textContent = humanScore;
+            computerScoreSpan.textContent = computerScore;
         }
         // else determine if user wins with success message returned
-        else if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" && computerChoice == 'rock') || (humanChoice =="scissors" && computerChoice == "paper")) {
+        else if ((playerSelection == "rock" && computerChoice == "scissors") || (playerSelection == "paper" && computerChoice == 'rock') || (playerSelection =="scissors" && computerChoice == "paper")) {
             humanScore++;
-            console.log(`You win ${humanChoice} beats ${computerChoice}`);
+            roundResult.textContent =`You win the round! ${playerSelection} beats ${computerChoice}`; 
+            humanScoreSpan.textContent = humanScore;
+            computerScoreSpan.textContent = computerScore;
         }
         //else user loses with failure message returned
         else {
             computerScore++;
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            roundResult.textContent = `You lose the round! ${computerChoice} beats ${playerSelection}`;
+            humanScoreSpan.textContent = humanScore;
+            computerScoreSpan.textContent = computerScore;
         }   
     }
-    // Play game 5 times
-    for (let i=0; i<5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    // Return text of who won
-    if (humanScore == computerScore) {return "The game is a draw!";}
-    else if (humanScore > computerScore) {return "The game winner is the user";}
-    else return "The game winner is the computer";
-
+    
+    // add event listener to the button group to play a round on each user option click
+    // announce the winner at 5 points
+    buttonGroup.addEventListener("click", (e) => {
+        switch(e.target.className) {
+            case "button-rock":
+                playRound("rock", getComputerChoice());
+                break;
+                case "button-paper":
+                    playRound("paper", getComputerChoice());
+                    break;
+                case "button-scissors":
+                    playRound("scissors", getComputerChoice());
+                    break;
+                    };        
+                    //check if one of the players won the game with 5 points and update DOM
+                    if (humanScore === 5) {
+                        buttonGroup.previousSibling.remove();
+                        buttonGroup.previousSibling.remove();
+                        buttonGroup.remove();
+                        winningResult.textContent = "Human wins the game!";
+                    }
+                    else if (computerScore === 5) {
+                        buttonGroup.previousSibling.remove();
+                        buttonGroup.previousSibling.remove();
+                        buttonGroup.remove();
+                        winningResult.textContent = "Computer wins the game!";
+                    }
+                })             
 }
-// Output the game winner
+            
+
+// get button group div reference
+const buttonGroup = document.querySelector(".button-group");
+
+// get the .round-result <p> reference
+const roundResult =  document.querySelector(".round-result");
+// get the .human-score <span> reference
+const humanScoreSpan =  document.querySelector(".human-score span");
+// get the .computer-score <span> reference
+const computerScoreSpan =  document.querySelector(".computer-score span");
+// get the .winning-result <p> reference
+const winningResult =  document.querySelector(".winning-result");
+
+// start game
 console.log(playGame());
